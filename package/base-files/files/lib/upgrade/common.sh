65e8745a471aa1e0279990c61b2d3d1c3dd45067
johnnyman727 <johnnyman727@gmail.com>
2016-03-07 17:35:50 -0800

    Modifies upgrade file to preserve spid and usbexecd through sysupgrade

--- package/base-files/files/lib/upgrade/common.sh
+++ package/base-files/files/lib/upgrade/common.sh
@@ -130,7 +130,7 @@ kill_remaining() { # [ <signal> ]
 		else 
 			case "$name" in
 				# Skip essential services
-				*procd*|*ash*|*init*|*watchdog*|*ssh*|*dropbear*|*telnet*|*login*|*hostapd*|*wpa_supplicant*|*nas*|*relayd*) : ;;
+				*procd*|*ash*|*init*|*watchdog*|*ssh*|*dropbear*|*telnet*|*login*|*hostapd*|*wpa_supplicant*|*nas*|*relayd*|*spid*|*usbexecd*) : ;;
 
 				# Killable process
 				*)
